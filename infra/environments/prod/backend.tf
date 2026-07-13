3108c8077993e7d8dfcf29df325f6bd76090e47b
manojreddy <manojmulukuri@outlook.com>
2026-07-13 21:01:56 +0530

    fixing the prod backend name

--- infra/environments/prod/backend.tf
+++ infra/environments/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.9.0"
 
   backend "s3" {
-    bucket         = "taskmaster-terraform-state" # from bootstrap output: state_bucket_name
+    bucket         = "manoj-taskmaster-terraform-state" # from bootstrap output: state_bucket_name
     key            = "prod/terraform.tfstate"     # env-scoped key — isolates prod's state from dev's
     region         = "us-east-1"
     dynamodb_table = "taskmaster-terraform-locks" # from bootstrap output: lock_table_name
